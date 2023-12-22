45f2451709cc1462ff4d31a5b6c15f993d0b9c75
Beterraba <ls6182315@gmail.com>
2023-12-22 09:07:04 -0300

    fix messages

--- api/src/controllers/page_controller.cr
+++ api/src/controllers/page_controller.cr
@@ -2,16 +2,20 @@ get "/" do |env|
   env.redirect "/travels_stops", 200
 end
 
-error 404 do
-  {"message": "resousce not found!"}.to_json
+error 400 do
+  {"message": "array of travels_stops is empty"}.to_json
+end
+
+error 401 do
+  {"message": "unauthorized"}.to_json
 end
 
 error 403 do
   {"message": "access forbidden!"}.to_json
 end
 
-error 401 do
-  {"message": "unauthorized"}.to_json
+error 404 do
+  {"message": "resousce not found!"}.to_json
 end
 
 error 500 do
